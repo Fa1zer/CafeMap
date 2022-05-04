53225edce8247e8d74f9208600e73a0a229629aa
WhyNotWhyYes <artemiy.zuzin@gmail.com>
2022-05-04 12:05:12 +0300

    Initial Commit

---
+++ CafeMap/CafeMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  CafeMapApp.swift
+//  CafeMap
+//
+//  Created by Artemiy Zuzin on 04.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct CafeMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
