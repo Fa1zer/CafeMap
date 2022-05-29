38f0512aae434db890e0bf0abb62ed0c3c1eec0f
WhyNotWhyYes <artemiy.zuzin@gmail.com>
2022-05-29 20:42:36 +0300

    keychain

--- PlacesMap/RegistrationModule/RegistrationModel.swift
+++ PlacesMap/RegistrationModule/RegistrationModel.swift
@@ -11,9 +11,11 @@ import Combine
 final class RegistrationModel {
     
     private let dataManager: DataManager
+    private let keychainManager: KeychainManager?
     
-    init(dataManager: DataManager) {
+    init(dataManager: DataManager, keychainManager: KeychainManager? = nil) {
         self.dataManager = dataManager
+        self.keychainManager = keychainManager
     }
     
     func logIn(email: String, passsword: String, didComplete: @escaping () -> Void, didNotComplete: @escaping (LogInErrors) -> Void) {
@@ -27,7 +29,19 @@ final class RegistrationModel {
     }
     
     func signIn(email: String, passsword: String, didComplete: @escaping () -> Void, didNotComplete: @escaping (SignInErrors) -> Void) {
-        self.dataManager.authUser(user: User(email: email, password: passsword), didComplete: didComplete, didNotComplete: didNotComplete)
+        let newDidComplete = {
+            didComplete()
+            
+            self.keychainManager?.setEmailAndPassword(email: email, password: passsword)
+        }
+        
+        self.dataManager.authUser(user: User(email: email, password: passsword), didComplete: newDidComplete, didNotComplete: didNotComplete)
+    }
+    
+    func keychainSignIn(didComplete: @escaping () -> Void) {
+        guard let (email, password) = self.keychainManager?.getEmailAndPassword() else { return }
+        
+        self.dataManager.authUser(user: User(email: email, password: password), didComplete: didComplete, didNotComplete: { _ in })
     }
     
 }
